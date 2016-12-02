2910e78ded90acdea440c5674a89540c4afb096c
Zhmayev Yaroslav <salaros@yandex.ru>
2016-12-02 22:02:05 +0300

    bumped OpenMcdf-2 version to 2.0.2

--- src/OpenMcdf.Portable/Properties/AssemblyInfo.cs
+++ src/OpenMcdf.Portable/Properties/AssemblyInfo.cs
@@ -24,7 +24,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.*")]
+[assembly: AssemblyVersion("2.0.2.*")]
 
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("OpenMcdfTest")]
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("OpenMcdf.Extensions")]
+[assembly: InternalsVisibleTo("OpenMcdfTest")]
+[assembly: InternalsVisibleTo("OpenMcdf.Extensions")]
