7dbb4c150fbc0949f3c8b4a22ea6deda3c788d93
salaros <salaros@yandex.ru>
2017-12-27 19:26:18 +0300

    trying to repair AppVeyor's test runner

--- tests/OpenMcdf.Test/AssetDeployer.cs
+++ tests/OpenMcdf.Test/AssetDeployer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using NUnit.Framework;
 
@@ -11,13 +12,17 @@ namespace OpenMcdf.Test
         {
             Directory.SetCurrentDirectory(TestContext.CurrentContext.WorkDirectory);
 
+            Console.WriteLine($"Test context working directory: {TestContext.CurrentContext.WorkDirectory}");
+            Console.WriteLine($"Test context test directory: {TestContext.CurrentContext.TestDirectory}");
+
             var codeBaseDir = new DirectoryInfo(TestContext.CurrentContext.TestDirectory);
-            var assetsDir = Path.Combine(codeBaseDir.FullName, "assets");
+            var testAssets = Path.Combine("tests", "assets");
+            var assetsDir = Path.Combine(codeBaseDir.FullName, testAssets);
             while (!Directory.Exists(assetsDir) &&
                    !Path.GetPathRoot(codeBaseDir.FullName).Equals(codeBaseDir.FullName))
             {
                 codeBaseDir = codeBaseDir.Parent;
-                assetsDir = Path.Combine(codeBaseDir.FullName, "assets");
+                assetsDir = Path.Combine(codeBaseDir.FullName, testAssets);
             }
 
             foreach (var assetPath in Directory.GetFiles(assetsDir))
